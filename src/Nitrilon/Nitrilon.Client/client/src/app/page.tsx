1fbc41cba2422f6e3c7f6a9b6382a7730def7306
WizedWasTaken <jegerfattig1@gmail.com>
2024-04-15 23:48:06 +0200

    changed web

--- src/Nitrilon/Nitrilon.Client/client/src/app/page.tsx
+++ src/Nitrilon/Nitrilon.Client/client/src/app/page.tsx
@@ -1,7 +1,24 @@
-"use client";
-import { useEffect, useState } from "react";
-import Image from "next/image";
+'use client';
 
+// Dependencies
+import { useEffect, useState, useCallback, useMemo } from 'react';
+import Image from 'next/image';
+import { useRouter } from 'next/navigation';
+
+// Components
+import {
+  Card,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card';
+import { Progress } from '@/components/ui/progress';
+import { toast } from 'sonner';
+import React from 'react';
+import { Alert, AlertDescription, AlertTitle } from '@/components/ui/alert';
+import { Button } from '@/components/ui/button';
+
+// Types
 type Event = {
   id: string;
   name: string;
@@ -10,102 +27,159 @@ type Event = {
   attendees: number;
 };
 
+// Variables
+const API_EVENTS_URL = 'https://localhost:7097/api/Events';
+const API_EVENT_RATING_URL = 'https://localhost:7097/api/EventRating';
+
 export default function Home() {
   const [events, setEvents] = useState<Event[]>([]);
   const [showModal, setShowModal] = useState(false);
   const [selectedEvent, setSelectedEvent] = useState<Event | null>(null);
+  const [progress, setProgress] = useState(0);
+  const [error, setError] = useState(false);
+  const router = useRouter();
 
-  function handleEventClick(event: Event) {
-    console.log("Event clicked", event);
+  const handleEventClick = useCallback((event: Event) => {
+    console.log('Event clicked', event);
     setEvents([]);
     setSelectedEvent(event);
-  }
-
-  async function handleGradeClick(grade: number) {
-    fetch(
-      `https://localhost:7097/api/EventRating?EventId=${selectedEvent?.id}&RatingId=${grade}`,
-      {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-      }
-    );
-
-    setShowModal(true);
-    setTimeout(() => {
-      setShowModal(false);
-    }, 3000);
-  }
-
-  // Fetch data from the server
-  useEffect(() => {
-    fetch("https://localhost:7097/api/Event")
-      .then((res) => res.json())
-      .then((data) => setEvents(data));
   }, []);
 
+  const handleGradeClick = useCallback(
+    async (grade: number) => {
+      await fetch(
+        `${API_EVENT_RATING_URL}?EventId=${selectedEvent?.id}&RatingId=${grade}`,
+        {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        }
+      );
+
+      setShowModal(true);
+      setTimeout(() => {
+        setShowModal(false);
+      }, 3000);
+    },
+    [selectedEvent]
+  );
+
+  useEffect(() => {
+    async function fetchEvents() {
+      setProgress(41);
+      const response = await fetch(API_EVENTS_URL);
+      setProgress(80);
+      if (!response.ok) {
+        setProgress(100);
+        toast.error('Der skete en teknisk fejl. Prøv igen senere', {
+          description: `${new Date().toLocaleString()}`,
+          action: {
+            label: 'Prøv igen',
+            onClick: () => fetchEvents(),
+          },
+        });
+        return setError(true);
+      }
+      const data = await response.json();
+      setTimeout(() => {
+        setProgress(100);
+      }, 1000);
+      setEvents(data);
+    }
+
+    fetchEvents();
+  }, []);
+
+  const formattedDate = useMemo(() => {
+    if (selectedEvent) {
+      return new Date(selectedEvent.date).toLocaleDateString();
+    }
+    return '';
+  }, [selectedEvent]);
+
+  const isLoading =
+    events.length === 0 && !selectedEvent && !showModal && progress < 100;
+
   return (
-    <main className="flex flex-wrap h-screen justify-around items-center gap-5 p-20">
-      {events.map((event) => (
-        <div
-          key={event.id}
-          className="border p-4 w-full md:w-3/12 h-[150px] cursor-pointer bg-gray-400 text-white rounded-md relative"
-          onClick={() => handleEventClick(event)}
-        >
-          <h2>{event.name}</h2>
-          <p>{new Date(event.date).toLocaleDateString()}</p>
-          <p>{event.description}</p>
-          <p>{event.attendees === -1 ? "Ingen" : event.attendees} deltagere</p>
-        </div>
-      ))}
+    <main className='flex flex-wrap h-screen justify-around items-center gap-5 p-20'>
+      {progress == 100 &&
+        events.map((event) => (
+          <Card
+            key={event.id}
+            onClick={() => handleEventClick(event)}
+            className='w-full md:w-3/12 cursor-pointer'
+          >
+            <CardHeader>
+              <CardTitle>{event.name}</CardTitle>
+              <CardDescription>{formattedDate}</CardDescription>
+              <CardDescription>{event.description}</CardDescription>
+              <CardDescription>
+                {event.attendees === -1 ? 'Ingen' : event.attendees} deltagere
+              </CardDescription>
+            </CardHeader>
+          </Card>
+        ))}
       {selectedEvent && !showModal && (
         <>
-          <h1 className="absolute top-5 w-full text-center text-4xl font-bold">
+          <h1 className='absolute top-5 w-full text-center text-4xl font-bold'>
             Hvad synes du om besøget?
           </h1>
           <button
-            className="w-[30%] h-[70%] relative"
+            className='w-[30%] h-[70%] relative'
             onClick={() => handleGradeClick(3)}
           >
             <Image
-              src="/images/emoji/happy.webp"
-              alt="Happy emoji"
-              layout="fill"
-              objectFit="contain"
+              src='/images/emoji/happy.webp'
+              alt='Happy emoji'
+              layout='fill'
+              objectFit='contain'
             />
           </button>
           <button
-            className="w-[30%] h-[70%] text-6xl relative"
+            className='w-[30%] h-[70%] text-6xl relative'
             onClick={() => handleGradeClick(2)}
           >
             <Image
-              src="/images/emoji/neutral.jpg"
-              alt="Neutral emoji"
-              layout="fill"
-              objectFit="contain"
+              src='/images/emoji/neutral.jpg'
+              alt='Neutral emoji'
+              layout='fill'
+              objectFit='contain'
             />
           </button>
           <button
-            className="w-[30%] h-[70%] text-6xl relative"
+            className='w-[30%] h-[70%] text-6xl relative'
             onClick={() => handleGradeClick(1)}
           >
             <Image
-              src="/images/emoji/sad.png"
-              alt="Sad emoji"
-              layout="fill"
-              objectFit="contain"
+              src='/images/emoji/sad.png'
+              alt='Sad emoji'
+              layout='fill'
+              objectFit='contain'
             />
           </button>
         </>
       )}
       {showModal && (
         <section>
-          <h1 className="text-5xl">Tak for din anmeldelse!</h1>
-          <p className="text-3xl text-center">Vi ses snart igen :D</p>
+          <h1 className='text-5xl'>Tak for din anmeldelse!</h1>
+          <p className='text-3xl text-center'>Vi ses snart igen :D</p>
         </section>
       )}
-      {!events.length && !selectedEvent && !showModal && <p>Loading...</p>}
+      {isLoading && (
+        <Progress
+          value={progress}
+          className='w-[60%]'
+        />
+      )}
+      {error && (
+        <Alert>
+          <AlertTitle>Der skete en teknisk fejl</AlertTitle>
+          <AlertDescription>
+            Der skete en teknisk fejl. Prøv igen senere
+          </AlertDescription>
+        </Alert>
+      )}
     </main>
   );
 }
