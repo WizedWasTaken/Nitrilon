532c91caa82701a6f6226c1b173e422931aebd18
WizedWasTaken <jegerfattig1@gmail.com>
2024-04-25 09:19:56 +0200

    crazy update

---
+++ src/Nitrilon/Nitrilon.Entities/EventRating.cs
@@ -0,0 +1,45 @@
+﻿namespace Nitrilon.Entities
+{
+    public class EventRating
+    {
+        private int id;
+        private int eventId;
+        private int ratingId;
+
+        public int Id
+        {
+            get => id;
+            set
+            {
+                if (value < 0)
+                    throw new ArgumentException("Id must be a positive number.");
+
+                id = value;
+            }
+        }
+
+        public int EventId
+        {
+            get => eventId;
+            set
+            {
+                if (eventId <= 0)
+                    throw new ArgumentException("EventId must be above 0");
+
+                eventId = value;
+            }
+        }
+
+        public int RatingId
+        {
+            get => ratingId;
+            set
+            {
+                if (ratingId <= 0)
+                    throw new ArgumentException("RatingId must be above 0");
+
+                ratingId = value;
+            }
+        }
+    }
+}
---
+++ src/Nitrilon/Nitrilon.Entities/Rating.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nitrilon.Entities
+{
+    public class Rating
+    {
+        private int id;
+        private int ratingValue;
+        private string description;
+
+        public Rating(int id, int ratingValue, string description)
+        {
+            Id = id;
+            RatingValue = ratingValue;
+            Description = description;
+        }
+
+        public int Id
+        {
+            get => id;
+            set
+            {
+                if (value < 0)
+                    throw new ArgumentException("Id must be a positive number.");
+
+                id = value;
+            }
+        }
+
+        public int RatingValue
+        {
+            get => ratingValue;
+            set
+            {
+                if (value < 0 || value > 3)
+                    throw new ArgumentException("RatingValue must be between 0 and 3.");
+
+                ratingValue = value;
+            }
+        }
+
+        public string Description
+        {
+            get => description;
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                    throw new ArgumentException("Description cannot be empty.");
+
+                description = value;
+            }
+        }
+
+    }
+}
